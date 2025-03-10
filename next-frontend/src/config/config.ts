f557800c270f75f0c5e6f2f787d5aea9738ec3d4
edwin kairu <edwin.macharia@gozem.co>
2025-03-10 13:12:03 +0300

    fix for backend url env variable loading in production environment

--- next-frontend/src/config/config.ts
+++ next-frontend/src/config/config.ts
@@ -1,5 +1,10 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL
-console.log("base url !!! ==>", BASE_URL)
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+const BASE_URL = publicRuntimeConfig?.NEXT_PUBLIC_BACKEND_URL;
+
+console.log("Base URL at runtime:", BASE_URL);
+
 export const USER_API_URL = `${BASE_URL}/user`;
 export const HOUSE_API_URL = `${BASE_URL}/house`;
 export const TENANT_API_URL = `${BASE_URL}/tenant`;
